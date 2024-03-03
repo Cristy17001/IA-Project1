a366b14c77345c0099143196a63642cb8db1d386
Cristiano Rocha <up202108813@g.uporto.pt>
2024-03-03 18:14:18 +0000

    Fixed error in weight logic

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-# %%
 import random
 import pandas as pd
 
-# %%
 class Airplane:
     def __init__(self):
         # Randomly generate fuel level between 1000 and 5000 liters
@@ -12,18 +10,15 @@ class Airplane:
         # Randomly generate expected landing time between 10 and 120 minutes from now
         self.expected_landing_time = random.uniform(10, 120)
 
-# %%
 def generate_airplane_stream(num_airplanes):
     airplane_stream = [(index, Airplane()) for index in range(num_airplanes)]
     return airplane_stream
 
-# %%
 def generate_single_solution(airplane_stream):
     shuffled_airplane_stream = airplane_stream.copy()
     random.shuffle(shuffled_airplane_stream)
     return shuffled_airplane_stream
 
-# %%
 ## Generate n_solutions possible solutions
 def generate_possible_solutions(sorted_airplane_stream, size_of_generation):
     solutions = []
@@ -34,25 +29,21 @@ def generate_possible_solutions(sorted_airplane_stream, size_of_generation):
 
     return solutions
 
-# %%
 def check_fuel_related_incident(airplane, current_time):
     consumed_fuel = airplane.fuel_consumption_rate * current_time
     current_fuel_level = airplane.arriving_fuel_level - consumed_fuel
     return not(current_fuel_level >= 60 * airplane.fuel_consumption_rate)
 
-# %%
 def get_fuel_time_left(airplane, current_time):
     consumed_fuel = airplane.fuel_consumption_rate * current_time
     current_fuel = airplane.arriving_fuel_level - consumed_fuel
-    return current_fuel * airplane.fuel_consumption_rate
+    return current_fuel / airplane.fuel_consumption_rate
 
-# %%
 def check_for_crash(airplane, current_time):
     consumed_fuel = airplane.fuel_consumption_rate * current_time
     current_fuel_level = airplane.arriving_fuel_level - consumed_fuel
     return current_fuel_level <= 0
 
-# %%
 def fitness_function(airplane_stream):
     current_time = 0
     reverse_fitness_score = 0
@@ -69,27 +60,23 @@ def fitness_function(airplane_stream):
             if check_for_crash(airplane, current_time): return -1
 
             # Check if the plane has a fuel related incident
-            fuel_incident_weight = 2 if check_fuel_related_incident(airplane, current_time) else 0
+            fuel_incident_weight = 0
+            if check_fuel_related_incident(airplane, current_time):
+                # In case of accident, subtract the remaining fuel time multiplied by 0.02, because the greater the remaining fuel time, the better
+                fuel_incident_weight = 2 - (get_fuel_time_left(airplane, current_time) * 0.02)
 
-            # In case of accident, add the remaining time with fuel consumption rate multiplied by 0.2
-            land_as_fast_as_possible_weight = 0
-            if fuel_incident_weight:
-                land_as_fast_as_possible_weight = get_fuel_time_left(airplane, current_time) * 0.2
-
-            # If the plane is late, add the difference between the current time and the expected landing time multiplied by 0.1
+            # If the plane is late, add the difference between the current time and the expected landing time multiplied by 0.01
             expected_landing_time_weight = 0
             if current_time > airplane.expected_landing_time:
-                expected_landing_time_weight = (current_time - airplane.expected_landing_time) * 0.1
+                expected_landing_time_weight = (current_time - airplane.expected_landing_time) * 0.01
 
-            reverse_fitness_score += fuel_incident_weight + land_as_fast_as_possible_weight + expected_landing_time_weight
+            reverse_fitness_score += fuel_incident_weight + expected_landing_time_weight
 
         # Add 3 minutes to the current time
         current_time += 3
 
     return reverse_fitness_score
 
-
-# %%
 ## To identify the airplanes, we can use the index of the list
 airplane_stream = generate_airplane_stream(100)
 
@@ -97,7 +84,6 @@ airplane_stream = generate_airplane_stream(100)
 # Sort the airplane_stream by expected landing time
 sorted_airplane_stream = sorted(airplane_stream, key=lambda x: x[1].expected_landing_time)
 
-# %%
 possible_solutions = generate_possible_solutions(sorted_airplane_stream, 2000)
 
 # Create an empty list to store the DataFrames of each solution
@@ -125,7 +111,6 @@ result_df = pd.concat(dfs).sort_values(by="Fitness score", ascending=True).round
 
 print(result_df)
 
-# %%
 
 
 
